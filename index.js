083690e0e5d14cc99c063e30433f5ee53a8f9155
Mustafa Ihsan Kabakcili <mustafaihsankabakcili@gmail.com>
2022-02-19 16:07:46 +0100

    calculator functions added

--- index.js
+++ index.js
@@ -1,19 +1,96 @@
-let button_ac = document.querySelector('#ac');
-let button_plus_minus = document.querySelector("#plus_minus");
-let button_modulus = document.querySelector("#modulus");
-let button_division = document.querySelector("#division");
-let button_multiplication = document.querySelector("#multiplication");
-let button_minus = document.querySelector("#minus");
-let button_plus = document.querySelector("#plus");
-let button_equal = document.querySelector("#equal");
-let button_dot = document.querySelector("#dot");
-let button_zero = document.querySelector("#zero");
-let button_one = document.querySelector("#one");
-let button_two = document.querySelector("#two");
-let button_three = document.querySelector("#three");
-let button_four = document.querySelector("#four");
-let button_five = document.querySelector("#five");
-let button_six = document.querySelector("#six")
-let button_seven = document.querySelector("#seven");
-let button_eight = document.querySelector("#eight");
-let button_nine = document.querySelector("#nine");
+let operator = "";
+let num1 = "";
+let num2 = "";
+let result;
+let button_container = document.querySelector(".button_container");
+let memory = document.querySelector('.memory p');
+let main_area = document.querySelector('.input_container input');
+
+
+button_container.addEventListener('click', function(e) {
+
+    // Number clicks
+    if (e.target.classList.contains("numbers")) {
+        if (operator.length === 0) {
+            num1 += e.target.innerHTML;
+            memory.innerHTML = num1;
+            main_area.value = num1;
+
+        } else {
+            num2 += e.target.innerHTML;
+            memory.innerHTML = `${num1} ${operator} ${num2}`; // Changing to num1 operator num2
+            main_area.value = operation(num1, num2, operator);
+
+        }
+
+    }
+
+    // Adding operators and brackets to memory 
+    if ((e.target.classList.contains("operators") || (e.target.classList.contains("modulus"))) && !e.target.classList.contains("equal")) {
+
+        if (operator.length !== 0) {
+            result = operation(num1, num2, operator);
+            num1 = result;
+            num2 = "";
+            operator = "";
+        }
+        operator = e.target.innerHTML;
+        if (e.target.innerHTML === "x" || e.target.innerHTML === "÷") {
+            memory.innerHTML = `(${memory.innerHTML})`; // Changing to (num1 operator num2)
+        }
+        memory.innerHTML += ` ${operator}`;
+        main_area.value = "";
+    }
+
+    // Operations for AC, =, ±
+    if (e.target.classList.contains("ac") || e.target.classList.contains("plus_minus") || e.target.classList.contains("equal")) {
+        operator = "";
+
+        switch (e.target.innerHTML) {
+            case "AC":
+                num1 = "";
+                num2 = "";
+                memory.innerHTML = "";
+                main_area.value = "";
+                break;
+            case "=":
+                num1 = main_area.value;
+                num2 = "";
+                memory.innerHTML = `${num1}`;
+                main_area.value = `${num1}`;
+                num2 = "";
+                break;
+            case "±":
+                num1 = -Number(main_area.value);
+                num2 = "";
+                memory.innerHTML = `${num1}`;
+                main_area.value = `${num1}`;
+                num2 = "";
+                break;
+            default:
+                break;
+        }
+    }
+
+});
+
+// Operations for +, -, *, /, %
+function operation(number1, number2, operator) {
+    number1 = Number(number1);
+    number2 = Number(number2);
+
+    switch (operator) {
+        case "+":
+            return number1 + number2;
+        case "-":
+            return number1 - number2;
+        case "x":
+            return number1 * number2;
+        case "÷":
+            return number1 / number2;
+        case "%":
+            return number1 % number2;
+        default:
+            return;
+    }
+}
